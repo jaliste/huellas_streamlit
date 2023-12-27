5d63de4cd2afa3732c78d42482a80b809f70157d
José Aliste <jose.aliste@gmail.com>
2023-12-26 21:50:17 -0300

    Partial workaround to password problem

--- password.py
+++ password.py
@@ -9,7 +9,7 @@ def check_password():
         """Checks whether a password entered by the user is correct."""
         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the password.
+            #del st.session_state["password"]  # Don't store the password.
         else:
             st.session_state["password_correct"] = False
 
